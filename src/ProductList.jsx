68368ee79425fad6b8a0e01236455d6bd88de830
Daniel <74140112+EBOD13@users.noreply.github.com>
2024-07-13 20:52:54 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,15 @@
 import React, { useState,useEffect } from 'react';
+import addItem from './CreatSlice';
 import './ProductList.css'
 function ProductList() {
-  
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -251,8 +259,22 @@ function ProductList() {
         </div>
 
         <div className="product-grid">
-
-
+        {plantsArray.map((category, index) => (
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                    <div className="product-title">{plant.name}</div>
+                    <img className="product-image" src={plant.image} alt={plant.name} />
+                    <div className='product-cost'> {plant.cost} </div>
+                    <div className='product-description'> {plant.description} </div>
+                    <button onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  </div>
+))}
         </div>
 
     </div>
